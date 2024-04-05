8b860116088e7328b30c66a724d2134b9cdb01f2
jpsandiego22 <jomersan0619@gmail.com>
2024-04-05 12:54:23 +0800

    first commit

--- src/component/PortfolioJson.jsx
+++ src/component/PortfolioJson.jsx
@@ -1,19 +1,19 @@
 import * as Icon from 'react-bootstrap-icons';
 const details =
     [
-        { id: 1, img:'/chsi.jpg', category: 'Website', url: 'https://caritashealthshield.com.ph/', dateHired: 'Jul 2018 - Nov 2022', 
+        { id: 1, img:'chsi.jpg', category: 'Website', url: 'https://caritashealthshield.com.ph/', dateHired: 'Jul 2018 - Nov 2022', 
             details:'Maintained & Build using the ff <br/> Laravel v5.5, MariaDB, JQuery, Ajax, JSON' },
-        { id: 2, img:'/chsi-mis.jpg', category: 'Website', url: 'https://caritashealthshield.com.ph/caritas_mis/', dateHired: 'Jul 2018 - Nov 2022', 
+        { id: 2, img:'chsi-mis.jpg', category: 'Website', url: 'https://caritashealthshield.com.ph/caritas_mis/', dateHired: 'Jul 2018 - Nov 2022', 
             details:'Maintained & Build using the ff<br/>Laravel v5.5, MariaDB, JQuery, Ajax, JSON' },
-        { id: 3, img:'/chsi-proposal.jpg', category: 'Website', url: 'https://caritashealthshield.com.ph/new_proposal/', dateHired: 'Jul 2018 - Nov 2022', 
+        { id: 3, img:'chsi-proposal.jpg', category: 'Website', url: 'https://caritashealthshield.com.ph/new_proposal/', dateHired: 'Jul 2018 - Nov 2022', 
             details:'Maintained & Build using the ff<br/>Laravel v5.5, MariaDB, JQuery, Ajax, JSON' },
-        { id: 4, img:'/issabelPBX.jpg', category: 'Website', url: 'https://www.issabel.org/get-issabel/', dateHired: 'Nov 2022 - Present',  
+        { id: 4, img:'issabelPBX.jpg', category: 'Website', url: 'https://www.issabel.org/get-issabel/', dateHired: 'Nov 2022 - Present',  
             details:'Modification of Issabel PBX Module (CampaignOut, CallsReports, Setup Extensions, Queuee Configuration, Callback Extensions & API Integration(Using Python))' },
-        { id: 5, img:'/nodejs-expressjs.jpg', category: 'Website', url: 'https://github.com/jpsandiego22/nodeJS-Crud-Jwt-BasicAuth-RestApi', dateHired: 'N/A', 
+        { id: 5, img:'nodejs-expressjs.jpg', category: 'Website', url: 'https://github.com/jpsandiego22/nodeJS-Crud-Jwt-BasicAuth-RestApi', dateHired: 'N/A', 
             details:'Build using NodeJS, ExpressJS, JSON Web Token & Basic Authorization - CRUD RestAPI.' },
-        { id: 6, img:'/react-works.jpg', category: 'Website', url: '/', dateHired: 'N/A', 
+        { id: 6, img:'react-works.jpg', category: 'Website', url: '/', dateHired: 'N/A', 
             details:'Maintained & Build using the ff<br/>Laravel v5.5, MariaDB, JQuery, Ajax, JSON' },
-        { id: 7, img:'/aeon-webadmin.jpg', category: 'Website', url: 'N/A', dateHired: 'N/A', 
+        { id: 7, img:'aeon-webadmin.jpg', category: 'Website', url: 'N/A', dateHired: 'N/A', 
             details:'Maintained & Build using the ff<br/>Laravel v5.5, MariaDB, JQuery, Ajax, JSON' }
            
     ];
