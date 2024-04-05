77d184fc1506b25832b130422f5643b0fe461119
jpsandiego22 <jomersan0619@gmail.com>
2024-04-05 13:45:23 +0800

    first commit

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import * as Icon from 'react-bootstrap-icons';
 import PortfolioModal from './PortfolioModal';
 import {projects} from './PortfolioJson';
+
 function Portfolio() {
     const [modalShow, setModalShow] = useState(false);
     const [targetId, settargetId] = useState(0);
@@ -9,21 +10,19 @@ function Portfolio() {
         projects.map(item => (
             <li key={item.id} className="col-lg-4 col-md-6 portfolio-item filter-app">
                 <div   className="portfolio-wrap rounded p-2">
-                    {/* <div> */}
-                        <img src={item.img} className="img-fluid" alt=""/>
-                        <div className="portfolio-info">
-                            <h4>{item.projectName}</h4>
-                            <p>{item.title}</p>
-                            <div className="portfolio-links">
-                                <a href='#' onClick={() => {setModalShow(true); settargetId(item.keys)}} data-gallery="portfolioDetailsGallery" data-glightbox="type: external" className="portfolio-details-lightbox" title="Project Details"><Icon.PlusCircle size={40} className='text-primary rounded p-1 mb-3'/></a>
-                                {item.keys==5 ? (<a href={item.url} target='_blank' data-gallery="portfolioDetailsGallery" data-glightbox="type: external" className="portfolio-details-lightbox" title="Project Link"><Icon.Github size={40} className='text-primary rounded p-1 mb-3'/></a>)
-                                :(<a href={item.url} target='_blank' data-gallery="portfolioDetailsGallery" data-glightbox="type: external" className="portfolio-details-lightbox" title="Project Link"><Icon.Link45deg size={50} className='text-primary rounded p-1 mb-3'/></a>)}
-                                
-                            </div>
+                    <img src={item.img} className="img-fluid" alt=""/>
+                    <div className="portfolio-info">
+                        <h4>{item.projectName}</h4>
+                        <p>{item.title}</p>
+                        <div className="portfolio-links">
+                            <a href='#' onClick={() => {setModalShow(true); settargetId(item.keys)}} data-gallery="portfolioDetailsGallery" data-glightbox="type: external" className="portfolio-details-lightbox" title="Project Details"><Icon.PlusCircle size={40} className='text-primary rounded p-1 mb-3'/></a>
+                            {item.keys==5 ? (<a href={item.url} target='_blank' data-gallery="portfolioDetailsGallery" data-glightbox="type: external" className="portfolio-details-lightbox" title="Project Link"><Icon.Github size={40} className='text-primary rounded p-1 mb-3'/></a>)
+                            :(<a href={item.url} target='_blank' data-gallery="portfolioDetailsGallery" data-glightbox="type: external" className="portfolio-details-lightbox" title="Project Link"><Icon.Link45deg size={50} className='text-primary rounded p-1 mb-3'/></a>)} 
                         </div>
-                    {/* </div> */}
+                    </div>
                 </div>
             </li>));
+
     return (
     <>
     
@@ -54,9 +53,9 @@ function Portfolio() {
                     </div>
                 </div>
             </div>
-           
+            <PortfolioModal show={modalShow} handleCloseModal={() =>setModalShow(false)} targetId={targetId}/>
         </div>
-         <PortfolioModal show={modalShow} handleCloseModal={() =>setModalShow(false)} targetId={targetId}/>
+        
     </>
     )
 }
